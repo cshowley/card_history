46980e95b659e0027c4a4e0295515ab3b6134225
Charlie Showley <cgshowley@gmail.com>
2025-11-26 22:26:11 -0800

    compile a bunch of ideas for features

---
+++ v2/train.py
@@ -0,0 +1,51 @@
+'''
+features
+
+200d moving average price
+50d moving average price
+macd value (???)
+last sale price
+previous 1 week average price
+previous 2 week average price
+previous 3 week average price
+previous 4 week average price
+grade
+grading company
+time series-derived price to current day
+
+(all values 1 grade below target)
+200d moving average price
+50d moving average price
+macd value
+last sale price
+previous 1 week average price
+previous 2 week average price
+previous 3 week average price
+previous 4 week average price
+time series-derived price to current day
+grading company (falls back to next-highest volume grader)
+
+(all values 1 grade above target)
+200d moving average price
+50d moving average price
+macd value
+last sale price
+previous 1 week average price
+previous 2 week average price
+previous 3 week average price
+previous 4 week average price
+time series-derived price to current day
+grading company (falls back to next-highest volume grader)
+
+**market price action, defined as top N cards and/or an index of popular cards
+previous 1 week price % change from previous week
+volume (number of sales)
+previous 2 week price % change from previous week
+volume (number of sales)
+previous 3 week price % change from previous week
+volume (number of sales)
+previous 4 week price % change from previous week
+volume (number of sales)
+
+
+'''
