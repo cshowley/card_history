d8d980b28bff16438a455bfd95d22976b770de88
Charlie Showley <cgshowley@gmail.com>
2025-12-01 16:55:55 -0800

    add a few feature columns

--- v2/train.py
+++ v2/train.py
@@ -1,9 +1,10 @@
 '''
 features
 
-200d moving average price
+26w moving average price
+12w moving average price
+200d moving average price (may be NaN unless we include data before 9/2025)
 50d moving average price
-macd value (???)
 last sale price
 previous 1 week average price
 previous 2 week average price
@@ -12,9 +13,15 @@ previous 4 week average price
 grade
 grading company
 time series-derived price to current day
-
+round half grades down to floor grade
+	include a binary feature for half grade or pristine/black cards
+	1 if half/pristine (e.g. 8.5)
+	0 if not half/pristine
+	
 (all values 1 grade below target)
-200d moving average price
+26w moving average price
+12w moving average price
+200d moving average price (may be NaN unless we include data before 9/2025)
 50d moving average price
 macd value
 last sale price
@@ -26,7 +33,9 @@ time series-derived price to current day
 grading company (falls back to next-highest volume grader)
 
 (all values 1 grade above target)
-200d moving average price
+26w moving average price
+12w moving average price
+200d moving average price (may be NaN unless we include data before 9/2025)
 50d moving average price
 macd value
 last sale price
